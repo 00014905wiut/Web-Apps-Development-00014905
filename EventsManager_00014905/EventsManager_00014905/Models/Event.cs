238a3c19d25e0dd5ecea557ed6aef662e04d68e6
00014905wiut <118118342+00014905wiut@users.noreply.github.com>
2024-02-03 19:46:37 +0500

    event added
    project started, event class added

---
+++ EventsManager_00014905/EventsManager_00014905/Models/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace EventsManager_00014905.Models
+{
+    public class Event
+    {
+        private int EventId { get; set; }
+        public string EventName { get; set; } = string.Empty;
+        public string EventDescription { get; set; }
+        public string EventCategory { get; set; }
+
+        
+
+    }
+}
